4e7593a183fe0ecc2b8114943af5460fcf4baae5
zoo1994 <composure1994@gmail.com>
2022-05-09 14:16:47 +0900

    1

---
+++ src/main/java/com/fd/s1/FinalDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.fd.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalDeliveryApplication.class, args);
+	}
+
+}
